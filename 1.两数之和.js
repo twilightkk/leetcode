07c7f92402a1365377bb1ea055b0615071b391ce
twilightkk <twilightkk@qq.com>
2021-10-07 17:59:46 +0800

    周四 2021/10/07

---
+++ 1.两数之和.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// 穷举法  时间O(n^2) 空间O(1)
+/* var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      let temp = target - nums[i]
+      if (temp == nums[j]) {
+        return [i, j]
+      }
+    }
+  }
+}; */
+
+// 使用map 时间O(n) 空间O(n)
+var twoSum = function (nums, target) {
+  let map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    let temp = target - nums[i]
+    if (map.has(temp)) {
+      return [map.get(temp), i]
+    }
+    map.set(nums[i], i)
+  }
+};
+// @lc code=end
+
---
+++ 26.删除有序数组中的重复项.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=javascript
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 过滤重复元素 但与题目不符合 时间O(n) 空间O(n)
+// set 的增删改查时间复杂度为O(1)； new set()时间复杂度应该为O(n)吧
+// 题外话 Array.include() 时间复杂度为O(n).
+/* var removeDuplicates = function (nums) {
+  let set = new Set(nums)
+  return Array.from(set)
+}; */
+
+//快慢指针 时间O(n) 空间O(1)
+var removeDuplicates = function (nums) {
+  let n = nums.length
+  if (n == 0) return 0
+  let slow = 1, fast = 1
+  while (fast < n) {
+    if (nums[fast] != nums[fast - 1]) {
+      nums[slow] = nums[fast]
+      ++slow
+    }
+    ++fast
+  }
+  return slow
+};
+// @lc code=end
+
