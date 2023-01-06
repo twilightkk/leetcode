1549002ebf54e12d2067cda667f5c240256f9594
twilightkk <twilightkk@qq.com>
2023-01-06 19:01:29 +0800

    周五 2023/01/06

--- 119.杨辉三角-ii.js
+++ 119.杨辉三角-ii.js
@@ -9,8 +9,21 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
-var getRow = function(rowIndex) {
 
+/**
+ * 继续118-杨辉三角特性
+ * 根据组合公式可知同一行相邻两项的关系为
+ * $ C_n^m=C_n^{m-1}\times\frac{n-m+1}{m} $
+ * 且 $  C_n^0=1 $
+*/ 
+
+var getRow = function(rowIndex) {
+  let rows= new Array(rowIndex+1).fill(0)
+  rows[0]=1;
+  for (let i = 1; i <= rowIndex; i++) {
+    rows[i]=rows[i-1]*(rowIndex-i+1)/i;
+  }
+  return rows
 };
 // @lc code=end
 
